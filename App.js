959718d89f12b053bcec3339dc09f8f18f094370
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-06-07 19:59:58 +0200

    get location

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 import Loading from './Loading';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    (async () => {
+      try {
+        await Location.requestForegroundPermissionsAsync();
+        const { coords: {latitude, longitude} } = await Location.getCurrentPositionAsync();
+        setIsLoading(false);
+      } catch (error) {
+        Alert.alert('Permission to access location', 'Was Denied !')
+      }
+      
+    })();
+  }, []);
+
   return (
-    <Loading />
+    isLoading ? <Loading /> : null
   );
 }
